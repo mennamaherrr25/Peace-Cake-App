882c57465ee4ed6e3174e53ca7dd85f9558fd8c5
memnamaherrr25 <memnamaherrr25@gmail.com>
2026-02-23 07:35:00 +0200

    build app apk

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,11 @@
+# YouTube Player
+-keep class com.pierfrancescosoffritti.androidyoutubeplayer.** { *; }
+-keep class com.google.android.youtube.player.** { *; }
+
+# Flutter
+-keep class io.flutter.** { *; }
+-keep class io.flutter.plugins.** { *; }
+
+# 🔥 Play Core
+-keep class com.google.android.play.core.** { *; }
+-dontwarn com.google.android.play.core.**
--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -28,101 +28,110 @@ class _HomeViewState extends State<HomeView> {
       appBar: AppBar(
         elevation: 0,
         titleSpacing: 0,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Row(
-            children: [
-              Icon(Icons.play_circle_fill, color: Colors.red, size: 26),
-              const SizedBox(width: 6),
+        title: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 420),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                children: [
+                  Icon(Icons.play_circle_fill, color: Colors.red, size: 26),
+                  const SizedBox(width: 6),
 
-              const Text(
-                'Peace Cake',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-              ),
+                  const Text(
+                    'Peace Cake',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
 
-              const SizedBox(width: 12),
+                  const SizedBox(width: 12),
 
-              // 🔍 Search field
-              Expanded(
-                child: TextField(
-                  controller: searchController,
-                  onChanged: searchVideos,
-                  decoration: InputDecoration(
-                    hintText: 'Search',
-                    filled: true,
-                    fillColor: Colors.grey.shade200,
-                    contentPadding: const EdgeInsets.symmetric(vertical: 8),
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
+                  // 🔍 Search field
+                  Expanded(
+                    child: TextField(
+                      controller: searchController,
+                      onChanged: searchVideos,
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        filled: true,
+                        fillColor: Colors.grey.shade200,
+                        contentPadding: const EdgeInsets.symmetric(vertical: 8),
+                        prefixIcon: const Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemCount: filteredList.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => VideoPlayerPage(
-                      videoId: filteredList[index]['id'],
-                      title: filteredList[index]['title'],
+      body: Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 420),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: filteredList.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => VideoPlayerPage(
+                          videoId: filteredList[index]['id'],
+                          title: filteredList[index]['title'],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    elevation: 2,
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        // boxShadow: [
+                        //   BoxShadow(blurRadius: 10, color: Colors.black26),
+                        // ],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: CachedNetworkImage(
+                              imageUrl:
+                                  'https://img.youtube.com/vi/${filteredList[index]['id']}/0.jpg',
+                              placeholder: (context, url) =>
+                                  Center(child: CircularProgressIndicator()),
+                              errorWidget: (context, url, error) =>
+                                  Icon(Icons.error),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              filteredList[index]['title'],
+                              style: TextStyle(fontSize: 18),
+                              textDirection: TextDirection.rtl,
+                              textAlign: TextAlign.start,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
               },
-              child: Card(
-                elevation: 2,
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    // boxShadow: [
-                    //   BoxShadow(blurRadius: 10, color: Colors.black26),
-                    // ],
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: CachedNetworkImage(
-                          imageUrl:
-                              'https://img.youtube.com/vi/${filteredList[index]['id']}/0.jpg',
-                          placeholder: (context, url) =>
-                              Center(child: CircularProgressIndicator()),
-                          errorWidget: (context, url, error) =>
-                              Icon(Icons.error),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Text(
-                          
-                          filteredList[index]['title'],
-                          style: TextStyle(fontSize: 18),
-                          textDirection: TextDirection.rtl,
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
+            ),
+          ),
         ),
       ),
     );
